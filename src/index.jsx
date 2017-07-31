accd1ccb93b206caba343d9b8124e98a41690372
Mike Doyle <mikedoyle007@gmail.com>
2017-07-31 14:18:12 -0700

    Create src/components and index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const destination = document.getElementById('App');
+
+ReactDOM.render(<App />, destination);

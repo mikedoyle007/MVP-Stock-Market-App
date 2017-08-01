7a4b0302eb09399c9d7605207a5ad50309b09d70
Mike Doyle <mikedoyle007@gmail.com>
2017-08-01 01:01:55 -0700

    Remove old code

--- static/bundle.js
+++ static/bundle.js
@@ -23638,22 +23638,6 @@ var App = function (_React$Component) {
     return _this;
   }
 
-  // componentDidMount() {
-  //   const api_key = 'uz2s6s5WS86ZeASb5qnE';
-  //   const url = `https://www.quandl.com/api/v3/datasets/WIKI/FB/data.csv?api_key=${api_key}`;
-  //   // query database
-  //   axios.get(url)
-  //     .then(({ data }) => {
-  //       //TODO: this should get results from database only
-  //       const price = data.split(',')[20];
-  //       console.log('response is : ', price);
-  //       // this.setState({stocks: ['Facebook', price]});
-  //     })
-  //     .catch((err) => {
-  //       console.log('error retrieving stock information ', err);
-  //     });
-  // }
-
   _createClass(App, [{
     key: 'handleChange',
     value: function handleChange(event) {
@@ -23706,49 +23690,6 @@ var App = function (_React$Component) {
 ;
 
 module.exports = App;
-
-/*
-handleChange (event) {
-    this.setState({
-      searchTerm: event.target.value
-    });
-    //TODO: this should send the user's input as a post request to the server
-    // app.post('/server', (req, res) => {
-      const api_key = 'uz2s6s5WS86ZeASb5qnE';
-      let userInput = this.state.searchTerm;
-      console.log('userinput = ', userInput);
-      const url = `https://www.quandl.com/api/v3/datasets/WIKI/${userInput}/data.csv?api_key=${api_key}`;
-
-      axios.get(url)
-      .then(({ data }) => {
-        const name = this.state.searchTerm;
-        const price = data.split(',')[20];
-        const stockList = this.state.stocks;
-        stockList.push([name, price]);
-
-        this.setState({
-          stocks: stockList
-        });
-
-        let stockEntry = new StockEntry({
-          name: name,
-          price: price
-        })
-        .save((err, response) => {
-          if (err) {
-            console.log('error inside handle change request to stock api', err);
-          }
-        });
-      })
-      .then(() => {
-        res.send(200);
-      })
-      .catch((err) => {
-        console.log('error retrieving stock information ', err);
-      });
-    // });
-  }
-*/
 
 /***/ }),
 /* 319 */
